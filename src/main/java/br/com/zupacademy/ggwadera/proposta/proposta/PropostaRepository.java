df4241a470fd10cfc363626464cf184bba908c81
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-05-05 17:35:51 -0300

    entidade Proposta e repositório

---
+++ src/main/java/br/com/zupacademy/ggwadera/proposta/proposta/PropostaRepository.java
@@ -0,0 +1,5 @@
+package br.com.zupacademy.ggwadera.proposta.proposta;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropostaRepository extends JpaRepository<Proposta, Long> {}
