eca6feae56cd59831d8a183738a026728d107f6d
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-05-05 18:07:22 -0300

    adicionado validador no controlador

--- src/main/java/br/com/zupacademy/ggwadera/proposta/proposta/PropostaController.java
+++ src/main/java/br/com/zupacademy/ggwadera/proposta/proposta/PropostaController.java
@@ -1,11 +1,11 @@
 package br.com.zupacademy.ggwadera.proposta.proposta;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -15,6 +15,8 @@ import java.net.URI;
 @RequestMapping("/propostas")
 public class PropostaController {
 
+  private final Logger logger = LoggerFactory.getLogger(PropostaController.class);
+
   private final PropostaRepository propostaRepository;
 
   @Autowired
@@ -22,9 +24,15 @@ public class PropostaController {
     this.propostaRepository = propostaRepository;
   }
 
+  @InitBinder
+  public void init(WebDataBinder binder) {
+    binder.addValidators(new DocumentoJaSolicitadoValidator(propostaRepository::existsByDocumento));
+  }
+
   @PostMapping
   public ResponseEntity<Void> novaProposta(@RequestBody @Valid NovaPropostaRequest request) {
     Proposta proposta = propostaRepository.save(request.toModel());
+    logger.info("Nova proposta {}", proposta);
     URI location =
         ServletUriComponentsBuilder.fromCurrentRequest()
             .path("/{id}")
--- src/main/java/br/com/zupacademy/ggwadera/proposta/proposta/PropostaRepository.java
+++ src/main/java/br/com/zupacademy/ggwadera/proposta/proposta/PropostaRepository.java
@@ -2,4 +2,6 @@ package br.com.zupacademy.ggwadera.proposta.proposta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PropostaRepository extends JpaRepository<Proposta, Long> {}
+public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+  boolean existsByDocumento(String documento);
+}
