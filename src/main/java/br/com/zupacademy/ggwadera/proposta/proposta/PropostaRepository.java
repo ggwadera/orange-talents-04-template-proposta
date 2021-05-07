1db5d691fb50fb3b533516dd6789a9907591d471
Guilherme Gwadera <16023489+ggwadera@users.noreply.github.com>
2021-05-07 09:58:37 -0300

    campo idCartao na entidade Proposta

--- src/main/java/br/com/zupacademy/ggwadera/proposta/proposta/PropostaRepository.java
+++ src/main/java/br/com/zupacademy/ggwadera/proposta/proposta/PropostaRepository.java
@@ -1,7 +1,13 @@
 package br.com.zupacademy.ggwadera.proposta.proposta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
 
 public interface PropostaRepository extends JpaRepository<Proposta, Long> {
+
   boolean existsByDocumento(String documento);
+
+  List<Proposta> findByIdCartaoIsNullAndEstadoIs(@NonNull EstadoProposta estado);
 }
